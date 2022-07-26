596605786f2bcf3f95fe0c8c2e5b24cd86a0058b
SAM4you <ander.stlouis@gmail.com>
2022-07-26 08:33:54 -0400

    Intent added, new activity called

--- app/src/main/java/com/ander/mymovies/models/Movie.java
+++ app/src/main/java/com/ander/mymovies/models/Movie.java
@@ -15,15 +15,21 @@ public class Movie {
     String posterPath;
     String title;
     String overview;
+    Double voteAverage;
 
     // no-arg, empty constructor required for Parceler
     public Movie() {}
+
+    public Double getVoteAverage() {
+        return voteAverage;
+    }
 
     public Movie(JSONObject jsonObject) throws JSONException {
         backdropPath = jsonObject.getString("backdrop_path");
         posterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
+        voteAverage = jsonObject.getDouble("vote_average");
     }
     public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
         List<Movie> movies = new ArrayList<>();
@@ -47,4 +53,6 @@ public class Movie {
     public String getOverview() {
         return overview;
     }
+
+
 }
